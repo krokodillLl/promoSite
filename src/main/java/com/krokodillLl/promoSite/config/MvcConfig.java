e47a9cfbb2fdb9fef5e062d5a94b79f7ec4093e9
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2020-02-06 01:10:50 +0300

    first step

---
+++ src/main/java/com/krokodillLl/promoSite/config/MvcConfig.java
@@ -0,0 +1,12 @@
+package com.krokodillLl.promoSite.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        
+    }
+}
---
+++ src/main/java/com/krokodillLl/promoSite/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.krokodillLl.promoSite.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    private final static Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/")
+    public String getMain(Model model) {
+
+        return "main";
+    }
+}
