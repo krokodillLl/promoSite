836d16040e9ecd47f710f33602dde4c03951498e
krokodillLl <53301367+krokodillLl@users.noreply.github.com>
2020-02-08 14:10:15 +0300

    the second step in creating a project page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,41 +1,41 @@
-var edu = new Vue({
-    el: '#education',
-    data: {
-        isShow: false,
-    },
-});
-
-var certif = new Vue({
-    el: '#certificates',
-    data: {
-        isShow: false,
-    },
-});
-
-var exp = new Vue({
-    el: '#experience',
-    data: {
-        isShow: false,
-    },
-});
-
-var stack = new Vue({
-    el: '#stack',
-    data: {
-        isShow: false,
-    },
-});
-
-var expect = new Vue({
-    el: '#expectations',
-    data: {
-        isShow: false,
-    },
-});
-
-var cont = new Vue({
-    el: '#contacts',
-    data: {
-        isShow: false,
-    },
-});
+// var edu = new Vue({
+//     el: '#education',
+//     data: {
+//         isShow: false,
+//     },
+// });
+//
+// var certif = new Vue({
+//     el: '#certificates',
+//     data: {
+//         isShow: false,
+//     },
+// });
+//
+// var exp = new Vue({
+//     el: '#experience',
+//     data: {
+//         isShow: false,
+//     },
+// });
+//
+// var stack = new Vue({
+//     el: '#stack',
+//     data: {
+//         isShow: false,
+//     },
+// });
+//
+// var expect = new Vue({
+//     el: '#expectations',
+//     data: {
+//         isShow: false,
+//     },
+// });
+//
+// var cont = new Vue({
+//     el: '#contacts',
+//     data: {
+//         isShow: false,
+//     },
+// });
---
+++ src/main/resources/static/js/projects.js
@@ -0,0 +1,13 @@
+var netcrackerProj = new Vue({
+    el:'#netcrackerProject',
+    data:{
+        isShow: false,
+    }
+});
+
+var snakeGame = new Vue({
+    el: '#snakeGame',
+    data: {
+        isShow: false,
+    }
+});
